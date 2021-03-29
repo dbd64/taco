5b63064c346dfbfca4d8454a739ea31b631a1530
Daniel Donenfeld <danielbd@csail.mit.edu>
2021-03-29 10:28:58 -0400

    Update benchmark and viewer

--- rle_test/main.cpp
+++ rle_test/main.cpp
@@ -12,14 +12,11 @@ std::default_random_engine gen(0);
 
 const Format dv({Dense});
 const Format rv({RLE});
-const IndexVar i("i"), j("j"), k("k");
 
 template <typename T = double>
-std::pair<Tensor<T>, Tensor<T>>
+Tensor<T>
 gen_random_rle(std::string name, int size = 100, int lower_rle = 1,
-               int upper_rle = 512, int lower = 0, int upper = 1, int rle_size_bits = 8) {
-  Format rv({RLE_s(rle_size_bits)});
-
+               int upper_rle = 512, int lower = 0, int upper = 1) {
   std::uniform_int_distribution<int> unif_rle(lower_rle, upper_rle);
   std::uniform_int_distribution<T> unif_vals(lower, upper);
 
@@ -29,7 +26,7 @@ gen_random_rle(std::string name, int size = 100, int lower_rle = 1,
     name = "_" + name + "_";
   }
 
-  Tensor<double> r("r" + name, {size}, rv);
+  Tensor<double> r("r" + name, {size}, dv);
   int index = 0;
   while (index < size) {
     int numCopies = min(unif_rle(gen), size - index);
@@ -40,62 +37,78 @@ gen_random_rle(std::string name, int size = 100, int lower_rle = 1,
     index += numCopies;
   }
   r.pack();
-
-  Tensor<double> d("d" + name, {size}, dv);
-  d(i) = r(i);
-  d.evaluate();
-
-  return {r, d};
+  return r;
 }
+
 std::vector<std::pair<int, int>> rle_ranges = //{{1,10}, {1,1024}, {1024,2048}};
-    {{1, 512}, {1, 1024}, {512, 1024}, {1000, 10000}, {10000, 100000}, {1, 10}};
+    {{1, 10}, {1, 1024}, {1000, 10000}};
 constexpr int size_lower = 10;
-constexpr int size_upper = 1'000'000;
+constexpr int size_upper = 10'000'000;
 constexpr int size_mult = 10;
 constexpr int val_lower = 1;
 constexpr int val_upper = 100;
 constexpr int val_mult = 100;
-constexpr int rle_bits_lower = 16;
-constexpr int rle_bits_upper = 32;
+constexpr int rle_bits_lower = 8;
+constexpr int rle_bits_upper = 64;
 constexpr int rle_bits_mult = 2;
 
-constexpr int numRandTensors = 1;
+constexpr int numRandTensors = 50;
 
 static void CustomArguments(benchmark::internal::Benchmark *b) {
-  for(int r = rle_bits_lower; r <= rle_bits_upper; r*= rle_bits_mult) {
-    for (int i = size_lower; i <= size_upper; i *= size_mult) { // Size of vector
-      for (int j = val_lower; j <= val_upper; j *= val_mult) {          // Upper limit of values
-        for (auto &x : rle_ranges) { // bounds on rle values
-          auto[l, u] = x;
-          b->Args({i, j, l, u, r, true});
-          b->Args({i, j, l, u, r, false});
+  for (int i = size_lower; i <= size_upper; i *= size_mult) { // Size of vector
+    for (int j = val_lower; j <= val_upper; j *= val_mult) {          // Upper limit of values
+      for (auto &x : rle_ranges) { // bounds on rle values
+        auto[l, u] = x;
+        b->Args({i, j, l, u, 0});
+        for(int r = rle_bits_lower; r <= rle_bits_upper; r*= rle_bits_mult) {
+          b->Args({i, j, l, u, r});
         }
       }
     }
   }
 }
 
-using vpTensor = std::vector<std::pair<Tensor<double>, Tensor<double>>>;
-
+using vpTensor = std::vector<Tensor<double>>;
 vpTensor current;
-
-bool isFilled;
-std::map<std::tuple<int, int, int, int, int>, vpTensor> ts;
-
-vpTensor& getCurrent(int tsize, int upperVal, int lRle, int uRle, int r, bool isDense){
-  assert(isDense == !isFilled);
-  if(!isFilled){
+vpTensor& updateCurrent(int tsize, int upperVal, int lRle, int uRle, bool isDense){
+  if(isDense){
     vpTensor vs;
     for (int k = 0; k < numRandTensors; k++) {
-      auto[r0, d0] = gen_random_rle<double>("0", tsize, lRle, uRle, 0, upperVal, r);
-      auto[r1, d1] = gen_random_rle<double>("1", tsize, lRle, uRle, 0, upperVal, r);
-      vs.push_back({r0, d0});
-      vs.push_back({r1, d1});
+      auto d0 = gen_random_rle<double>("0", tsize, lRle, uRle, 0, upperVal);
+      auto d1 = gen_random_rle<double>("1", tsize, lRle, uRle, 0, upperVal);
+
+      vs.push_back(d0);
+      vs.push_back(d1);
     }
     current = vs;
   }
-  isFilled = !isFilled;
   return current;
+}
+
+vpTensor getCurrentR(int tsize, int r) {
+  if(r == 0) {
+    return current;
+  }
+
+  Format rv({RLE_s(r)});
+
+  vpTensor vs;
+  bool toggle = true;
+  std::string name0("r0");
+  std::string name1("r1");
+
+  for(auto& d : current){
+    std::string name = toggle? name0: name1;
+    toggle = !toggle;
+    Tensor<double> r(name, {tsize}, rv);
+    IndexVar i("i");
+    r(i) = d(i);
+    r.evaluate();
+
+    vs.push_back(r);
+  }
+
+  return vs;
 }
 
 static void BM_all(benchmark::State &state) {
@@ -104,34 +117,31 @@ static void BM_all(benchmark::State &state) {
   int lRle = state.range(2);
   int uRle = state.range(3);
   int r = state.range(4);
-  bool isDense = state.range(5);
+  bool isDense = r == 0;
   Format rv({RLE_s(r)});
 
-  auto& res = getCurrent(tsize, upperVal, lRle, uRle, r ,isDense);
-
-  std::vector<Tensor<double>> es(numRandTensors);
-  for (int j = 0; j < numRandTensors; j++) {
-    state.PauseTiming();
-    auto [r0, d0] = res[2 * j];
-    auto [r1, d1] = res[2 * j + 1];
-    Tensor<double> expected("expected_", {tsize}, isDense? dv : rv);
-    if(isDense) {
-      expected(i) = d0(i) + d1(i);
-    } else {
-      expected(i) = r0(i) + r1(i);
-    }
-    expected.compile();
-    expected.assemble();
-    es.push_back(expected);
-  }
+  updateCurrent(tsize, upperVal, lRle, uRle, isDense);
+  auto res = getCurrentR(tsize, r);
 
   for (auto _ : state) {
-    for (auto& expected : es) {
-      expected.computeAndClear();
+    for (int j = 0; j < numRandTensors; j++) {
+      state.PauseTiming();
+      auto d0 = res[2 * j];
+      auto d1 = res[2 * j + 1];
+      Tensor<double> expected("expected_", {tsize}, isDense ? dv : rv);
+      const IndexVar i("i");
+      expected(i) = d0(i) + d1(i);
+      if (!isDense) {
+        expected.setAssembleWhileCompute(true);
+      }
+      expected.compile();
+      expected.assemble();
+      state.ResumeTiming();
+      expected.compute();
     }
   }
 }
 
-BENCHMARK(BM_all)->Apply(CustomArguments);
+BENCHMARK(BM_all)->Apply(CustomArguments)->ArgNames({"size", "value_upper", "rle_lower", "rle_upper", "rle_bits"})->MeasureProcessCPUTime()->Unit(benchmark::kMicrosecond); //->Repetitions(10)->ReportAggregatesOnly(false)->DisplayAggregatesOnly(false);
 
 BENCHMARK_MAIN();
--- rle_test/view_data.py
+++ rle_test/view_data.py
@@ -5,84 +5,159 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 path_res = '/Users/danieldonenfeld/Developer/taco/rle_test/bench_vec_out.json'
-path_out = '/Users/danieldonenfeld/Developer/taco/rle_test/plots_vec_compute/'
+path_out = '/Users/danieldonenfeld/Developer/taco/rle_test/plots_vec_rlebits_1/'
 
 # path_res = '/Users/danieldonenfeld/Developer/taco/rle_test/bench_mat_out.json'
 # path_out = '/Users/danieldonenfeld/Developer/taco/rle_test/plots_mat/'
+
+
+def split_name_str(s):
+    res = s.split(':')
+    return (res[0], res[1])
+
+
+def to_str(lst):
+    s = ""
+    for e in lst:
+        s += str(e) + ","
+    return s
+
+
+def process_benchmark(b, d, s, sg):
+    name = b['name']
+    name = name.split('/')
+
+    _, size = split_name_str(name[1])
+    vals_l = 0
+    _, vals_u = split_name_str(name[2])
+    _, rle_l = split_name_str(name[3])
+    _, rle_u = split_name_str(name[4])
+    _, rle_bits = split_name_str(name[5])
+
+    s.add(rle_bits)
+    sg.add(to_str([vals_l, vals_u, rle_l, rle_u]))
+
+    d_key = to_str([vals_l, vals_u, rle_l, rle_u, rle_bits])
+    xs, ys = d.setdefault(d_key, ([], []))
+    xs.append(size)
+    ys.append(b['cpu_time'])
+
+
+def add_bars(ax, xs_all, ys_lists, ind, width):
+    rects = []
+    i = 0
+    n = len(ys_lists)
+    k = 0
+    for ys in ys_lists:
+        lbl = "dense" if i == 0 else "rle " + str(i) + " bits"
+        rects.append(ax.bar(ind - ((n-k)*width/n) +
+                            width/2, ys, width/n, label=lbl))
+        k += 1
+        i = 8 if i == 0 else i*2
+    return rects
+
 
 with open(path_res) as f:
     data = json.load(f)
 
 d = dict()
+s = set()
+sg = set()
 
 for b in data['benchmarks']:
-    name = b['name']
-    name = name.split('/')
+    process_benchmark(b, d, s, sg)
 
-    case = name[0]
-    size = name[1]
-    vals_l = 0
-    vals_u = name[2]
-    rle_l = name[3]
-    rle_u = name[4]
+rle_bits_l = list(s)
+rle_bits_l.sort()
 
-    d_key = "values: [" + str(vals_l) + "," + str(vals_u) + "]" + \
-        ", run lengths: [" + str(rle_l) + "," + str(rle_u) + "]"
-
-    if 'dense' in case:
-        dense_data_x, dense_data_y, _, _ = d.setdefault(
-            d_key, ([], [], [], []))
-        dense_data_x.append(size)
-        dense_data_y.append(b['cpu_time'] / 1e6)  # convert to ms
-    else:
-        _, _, rle_data_x, rle_data_y = d.setdefault(d_key, ([], [], [], []))
-        rle_data_x.append(size)
-        rle_data_y.append(b['cpu_time'] / 1e6)  # convert to ms
-
-
-def autolabel(rects_l, rects_r, data_l, data_r):
-    data_zip = zip(data_l, data_r)
-    labels = list(map(lambda x: "{:10.2f}".format(x[0]/x[1]), data_zip))
-
-    for l, r, v in zip(rects_l, rects_r, labels):
-        height = max(l.get_height(), r.get_height())
-        ax.annotate('{}'.format(v),
-                    # xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xy=(r.get_x() - r.get_width()/4, height),
-                    xytext=(0, 2),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-
-for k, v in d.items():
-    print(k)
+for g in sg:
+    # Each iteration should produce one graph
+    print(g)
 
     fig, ax = plt.subplots()
 
-    dense_data_x, dense_data_y, rle_data_x, rle_data_y = v
+    data = []
+    for bits in rle_bits_l:
+        g_key = g + str(bits) + ","
+        xs, ys = d[g_key]
+        data.append((xs, ys))
 
-    if not (dense_data_x == rle_data_x):
-        print("ERROR! x values not same")
+    g = g.split(',')
 
-    N = len(dense_data_x)
+    xs_all = data[0][0]
+    for xs, ys in data:
+        if not (xs == xs_all):
+            print("ERROR! x values not same")
+
+    ys_lists = []
+    for xs, ys in data:
+        ys_lists.append(ys)
+
+    N = len(xs_all)
     ind = np.arange(N)
-    width = 0.4
+    width = 0.6
+    num_bars = len(rle_bits_l)
+    labels = list(map(lambda x: "{:.0e}".format(int(x)), xs_all))
 
-    labels = list(map(lambda x: "{:.0e}".format(int(x)), dense_data_x))
-
-    rects_dense = ax.bar(ind - width/2, dense_data_y, width, label="dense")
-    rects_rle = ax.bar(ind + width/2, rle_data_y, width, label='RLE')
+    rects = add_bars(ax, xs_all, ys_lists, ind, width)
     ax.set_yscale('log')
-    # ax.set_xlabel('Vector Length')
-    ax.set_xlabel('Matrix Size')
-    ax.set_ylabel('Time (ms)')
-    ax.set_title(k)
+    ax.set_xlabel('Vector Length')
+    ax.set_ylabel('Time (us)')
+    ax.set_title("Values: [" + g[0] + "," + g[1] +
+                 "], RLE: [" + g[2] + "," + g[3] + "]")
     ax.set_xticks(ind)
     ax.set_xticklabels(labels)
     ax.legend(loc="best")
 
-    autolabel(rects_dense, rects_rle, dense_data_y, rle_data_y)
-
     fig.tight_layout()
-    plt.savefig(path_out+k+".png")
+    plt.savefig(path_out+"Values_" + g[0] + "_" + g[1] + "__rle_" + g[2] + "_"
+                + g[3] + ".png")
     plt.close()
+
+
+# def autolabel(rects_l, rects_r, data_l, data_r):
+#     data_zip = zip(data_l, data_r)
+#     labels = list(map(lambda x: "{:10.2f}".format(x[0]/x[1]), data_zip))
+
+#     for l, r, v in zip(rects_l, rects_r, labels):
+#         height = max(l.get_height(), r.get_height())
+#         ax.annotate('{}'.format(v),
+#                     # xy=(rect.get_x() + rect.get_width() / 2, height),
+#                     xy=(r.get_x() - r.get_width()/4, height),
+#                     xytext=(0, 2),  # 3 points vertical offset
+#                     textcoords="offset points",
+#                     ha='center', va='bottom')
+
+
+# for k, v in d.items():
+#     print(k)
+
+#     fig, ax = plt.subplots()
+
+#     dense_data_x, dense_data_y, rle_data_x, rle_data_y = v
+
+#     if not (dense_data_x == rle_data_x):
+#         print("ERROR! x values not same")
+
+#     N = len(dense_data_x)
+#     ind = np.arange(N)
+#     width = 0.4
+
+#     labels = list(map(lambda x: "{:.0e}".format(int(x)), dense_data_x))
+
+#     rects_dense = ax.bar(ind - width/2, dense_data_y, width, label="dense")
+#     rects_rle = ax.bar(ind + width/2, rle_data_y, width, label='RLE')
+#     ax.set_yscale('log')
+#     # ax.set_xlabel('Vector Length')
+#     ax.set_xlabel('Matrix Size')
+#     ax.set_ylabel('Time (ms)')
+#     ax.set_title(k)
+#     ax.set_xticks(ind)
+#     ax.set_xticklabels(labels)
+#     ax.legend(loc="best")
+
+#     autolabel(rects_dense, rects_rle, dense_data_y, rle_data_y)
+
+#     fig.tight_layout()
+#     plt.savefig(path_out+k+".png")
+#     plt.close()
