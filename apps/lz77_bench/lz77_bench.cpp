07b438ca7a895ee3a6ee74e56a98bdb9155f7112
Daniel Donenfeld <danielbd@csail.mit.edu>
2021-07-01 15:06:29 -0400

    More small modifications to benchmark

--- apps/lz77_bench/lz77_bench.cpp
+++ apps/lz77_bench/lz77_bench.cpp
@@ -76,27 +76,32 @@ gen_random_lz77(std::string name, int size, double uncompressed_threshold,
   std::vector<int> runs;
   std::vector<T> vals;
 
-  int numRemaining = size;
+  int numRemaining = size-1;
   while(numRemaining > 0){
+    vals.push_back((T) unif_vals(gen));
     if (unif_compressed(gen) > uncompressed_threshold) {
-      auto run = std::min(unif_runs(gen), numRemaining);
-      dist.push_back(unif_dist(gen));
+      auto run = std::min(unif_runs(gen), numRemaining-1);
+      auto dist_v = std::min(unif_dist(gen), (int) vals.size());
+      if (run == 0) dist_v = 0;
+      dist.push_back(dist_v);
       runs.push_back(run);
-      vals.push_back((T) unif_vals(gen));
-      numRemaining -= run;
+      numRemaining -= run+1;
     } else {
       dist.push_back(0);
       runs.push_back(0);
-      vals.push_back((T) unif_vals(gen));
       numRemaining -= 1;
     }
   }
+
+  vals.push_back((T) unif_vals(gen));
+  dist.push_back(0);
+  runs.push_back(0);
 
   std::vector<int> pos = {0, static_cast<int>(vals.size())};
   return makeLZ77("lz77_"+name, {size}, pos, dist, runs, vals);
 }
 
-constexpr int size_lower = 1'000;
+constexpr int size_lower = 10;
 constexpr int size_upper = 100'000'000;
 constexpr int size_mult = 10;
 
@@ -105,138 +110,52 @@ constexpr int minElements = 10'000'000;
 
 static void CustomArguments(benchmark::internal::Benchmark *b) {
   for (int size = size_lower; size <= size_upper; size *= size_mult) { // Size of vector
-    for (int thresh = 0; thresh <= 10; thresh += 1) {
-      b->Args({size, thresh, false});
-//      b->Args({size, thresh, true});
+    for (int run = 1'000; run <= 1'000'000; run *= 10) {
+      b->Args({size, 0, run, true});
+      for (int thresh = 0; thresh <= 10; thresh += 5) {
+        b->Args({size, thresh, run, false});
+      }
     }
   }
 }
 
-using vpTensor = std::vector<Tensor<uint8_t>>;
-vpTensor current;
-vpTensor& updateCurrent(int tsize, double thresh, bool isDense){
-  if(!isDense){
-    vpTensor vs;
-    numRandTensors = std::max(minElements/tsize, 1);
-    for (int k = 0; k < numRandTensors; k++) {
-      auto d0 = gen_random_lz77("t0", tsize, thresh, 1, 1'000, 1, 10'000, 0, 255);
-      auto d1 = gen_random_lz77("t1", tsize, thresh, 1, 1'000, 1, 10'000, 0, 255);
-      vs.push_back(d0);
-      vs.push_back(d1);
-    }
-    current.swap(vs);
-  }
-  return current;
-}
-
-std::string name0("r0");
-std::string name1("r1");
-std::string name2("s0");
-std::string name3("s1");
-bool name_toggle = false;
-
-vpTensor& getCurrentR(int tsize, double thresh, bool isDense) {
-  if(!isDense) {
-    return current;
-  }
-
-  if(current.empty()){
-    std::cout << "WARNING!" << std::endl;
-    updateCurrent(tsize, thresh, false);
-  }
-
-  vpTensor vs;
-  bool toggle = true;
-  auto copy = getCopyFunc();
-
-  for(auto& d : current){
-    std::string name = name_toggle ? (toggle? name0: name1) : (toggle? name2: name3);
-    toggle = !toggle;
-    Tensor<uint8_t> r(name, {tsize}, dv);
-    IndexVar i("i");
-    r(i) = copy(d(i));
-    r.setAssembleWhileCompute(true);
-    r.compile();
-    r.compute();
-    vs.push_back(r);
-  }
-
-  name_toggle = !name_toggle;
-  current.swap(vs);
-
-  return current;
-}
-
-//static void BM_all(benchmark::State &state) {
-//  int tsize = state.range(0);
-//  double thresh = state.range(1)/10.0;
-//  bool isDense = state.range(2);
-//  auto plus_ = getPlusFunc();
-//
-//  updateCurrent(tsize, thresh, isDense);
-//  auto res = getCurrentR(tsize, thresh, isDense);
-//  int numTensors = res.size()/2;
-//
-//  for (auto _ : state) {
-//    for (int j = 0; j < numTensors; j++) {
-//      state.PauseTiming();
-//      auto d0 = res[2 * j];
-//      auto d1 = res[2 * j + 1];
-//      Tensor<uint8_t> expected("expected_", {tsize}, isDense ? dv : lz77f);
-//      const IndexVar i("i");
-//      expected(i) = plus_(d0(i), d1(i));
-//      expected.setAssembleWhileCompute(true);
-//      expected.compile();
-//      state.ResumeTiming();
-//      expected.compute();
-//    }
-//  }
-//
-//  int t0_vals_size_total = 0;
-//  int t1_vals_size_total = 0;
-//  for (int j = 0; j < numTensors; j++) {
-//    state.PauseTiming();
-//    auto d0 = res[2 * j];
-//    auto d1 = res[2 * j + 1];
-//    if(isDense){
-//      t0_vals_size_total += tsize;
-//      t1_vals_size_total += tsize;
-//    } else {
-//      taco_tensor_t *t0 = d0.getStorage();
-//      t0_vals_size_total += ((int32_t *)(t0->indices[0][0]))[1];
-//
-//      taco_tensor_t *t1 = d0.getStorage();
-//      t1_vals_size_total += ((int32_t *)(t1->indices[0][0]))[1];
-//    }
-//  }
-//
-//  state.counters.insert({{"num_tensors", numTensors},
-//                         {"t0_vals_size_total", t0_vals_size_total},
-//                         {"t1_vals_size_total", t1_vals_size_total}});
-//
-//}
 
 static void BM_all(benchmark::State &state) {
   int tsize = state.range(0);
   double thresh = state.range(1)/10.0;
-  bool isDense = state.range(2);
+  int run_upper = state.range(2);
+  bool isDense = state.range(3);
   auto plus_ = getPlusFunc();
+  auto copy = getCopyFunc();
 
-  // std::default_random_engine gen2(SEED);
-  // gen = gen2;
+  auto d0 = gen_random_lz77("t0", tsize, thresh, 1, 50, run_upper-100, run_upper, 0, 255);
+  auto d1 = gen_random_lz77("t1", tsize, thresh, 1, 50, run_upper-100, run_upper, 0, 255);
 
-  auto d0 = gen_random_lz77("t0", tsize, thresh, 1, 1'000, 1'000, 10'000, 0, 255);
-  auto d1 = gen_random_lz77("t1", tsize, thresh, 1, 1'000, 1'000, 10'000, 0, 255);
+  if (isDense){
+    Tensor<uint8_t> t0("t0_d", {tsize},   dv, 0);
+    Tensor<uint8_t> t1("t1_d", {tsize},   dv, 0);
+    const IndexVar i("i");
+
+    t0(i) = copy(d0(i));
+    t0.setAssembleWhileCompute(true);
+    t0.compile();
+    t0.compute();
+    d0 = t0;
+
+    t1(i) = copy(d1(i));
+    t1.setAssembleWhileCompute(true);
+    t1.compile();
+    t1.compute();
+    d1 = t1;
+  }
 
   for (auto _ : state) {
     state.PauseTiming();
-
     Tensor<uint8_t> expected("expected_", {tsize}, isDense ? dv : lz77f);
     const IndexVar i("i");
     expected(i) = plus_(d0(i), d1(i));
     expected.setAssembleWhileCompute(true);
     expected.compile();
-
     state.ResumeTiming();
 
     expected.compute();
@@ -247,8 +166,5 @@ BENCHMARK(BM_all)->Apply(CustomArguments)\
                  ->MeasureProcessCPUTime()\
                  ->Unit(benchmark::kMicrosecond)\
                  ->Repetitions(10);
-//                 ->ArgNames({"size", "value_upper", "rle_lower", "rle_upper", "rle_bits"})\
-;
-//->Repetitions(10)->ReportAggregatesOnly(false)->DisplayAggregatesOnly(false);
 
 BENCHMARK_MAIN();
